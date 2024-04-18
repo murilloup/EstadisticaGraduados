5c27ded0342cfc62bc4423ddc0a6f05a58c59b19
David Murillo <david.josue.murillo23@gmail.com>
2024-04-18 11:55:37 -0500

    Estructura del proyecto

---
+++ src/index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="./output.css" rel="stylesheet">
+   <title>Estadistica de Graduados</title>
+</head>
+
+<body class="container bg-gray-900">
+   
+   <h1 class="text-4xl text-slate-200 font-bold">
+      Hello world!
+   </h1>
+   
+   <script src="./src/app.js"></script>
+</body>
+</html>
